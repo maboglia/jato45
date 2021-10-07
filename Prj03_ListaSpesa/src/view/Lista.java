a3499ad1775ecb6bc1c17029405d03a1e09eeecf
maboglia <mauro.bogliaccino@gmail.com>
2021-10-07 15:39:31 +0200

    aggiorna

--- Prj03_ListaSpesa/src/view/Lista.java
+++ Prj03_ListaSpesa/src/view/Lista.java
@@ -10,15 +10,72 @@ public class Lista {
 	
 	public static void main(String[] args) {
 
-		Note taccuinoRosso = new Note(5);
+		Note taccuinoRosso = null;
+//		
+//		taccuinoRosso.addNota("pane", 0);
+//		taccuinoRosso.addNota("vino", 1);
+//		taccuinoRosso.addNota("latte", 2);
+//		taccuinoRosso.addNota("biscotti", 3);
+//		taccuinoRosso.addNota("olio", 4);
+//		
+//		taccuinoRosso.stampaLista();
+	
+		boolean b = 5<6;
+		char scelta;
 		
-		taccuinoRosso.addNota("pane", 0);
-		taccuinoRosso.addNota("vino", 1);
-		taccuinoRosso.addNota("latte", 2);
-		taccuinoRosso.addNota("biscotti", 3);
-		taccuinoRosso.addNota("olio", 4);
 		
-		taccuinoRosso.stampaLista();
+		while (b) {
+			scelta = askUser();
+			
+			if (scelta == 'q') {
+				b = false;
+				//return;//istruzione che interrompe
+				//break;//label che interrompe i cicli
+				System.out.println("Fine.....................");
+			}
+			else {
+				if (scelta == 'a') {
+					taccuinoRosso = new Note(5);
+				} 
+				if (scelta == 'b') {
+					taccuinoRosso.addNota("pane", 0);;
+				} 
+				if (scelta == 'c') {
+					//taccuinoRosso.completaNota(0);
+				} 
+				if (scelta == 'd') {
+					taccuinoRosso.stampaLista();
+				} 
+					
+				
+				
+			}
+			
+			
+		}
+	
 	}
+
+	private static char askUser() {
+		System.out.println("---------------------");
+		System.out.println("Scegli un'operazione");
+		System.out.println("a) crea lista");
+		System.out.println("b) crea nota");
+		System.out.println("c) completa nota");
+		System.out.println("d) stampa lista");
+		System.out.println("q) esci");
+		System.out.println("---------------------");
+		Scanner sc = new Scanner(System.in);
+		char risposta = sc.nextLine().charAt(0);
+		return risposta;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
