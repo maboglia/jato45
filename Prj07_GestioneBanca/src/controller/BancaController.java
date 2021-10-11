17ba72414b9ed3b2b07a8caf44f2c1aaa7cc3f9c
maboglia <mauro.bogliaccino@gmail.com>
2021-10-11 17:00:41 +0200

    aggiornamento banca

--- Prj07_GestioneBanca/src/controller/BancaController.java
+++ Prj07_GestioneBanca/src/controller/BancaController.java
@@ -16,10 +16,11 @@ public class BancaController {
 	public void addCliente(String nome, String cognome) {
 		int pos = trovaPostoLibero();
 		if (pos > -1) {
-		Cliente temp = new Cliente(nome, cognome);
-		System.out.println("Cliente aggiunto con successo!");
+			Cliente temp = new Cliente(nome, cognome);
+			clienti[pos] = temp;
+			//System.out.println("Cliente aggiunto con successo!");
 		} else {
-		System.out.println("Cliente NON aggiunto!");
+			System.out.println("Cliente NON aggiunto!");
 		}
 	}
 	
@@ -27,6 +28,7 @@ public class BancaController {
 		int pos = trovaPostoContiCorrenti();
 		if (pos > -1) {
 			ContoCorrente temp = new ContoCorrente(c);
+			conti[pos] = temp;
 			System.out.println("Conto aggiunto con successo");
 		} else {
 			System.out.println("Conto NON aggiunto");
@@ -50,4 +52,47 @@ public class BancaController {
 		return -1;
 	}
 
+	public int trovaCliente(String nome, String cognome) {
+		
+		for (int i = 0; i < clienti.length; i++) {
+			if (clienti[i] != null) {
+				if (clienti[i].getNome().equals(nome + " " + cognome)) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public Cliente trovaCliente(int id) {
+		return clienti[id];
+	}
+	
+	
+	public void reportClienti() {
+		System.out.println("Elenco clienti aggiornato");
+		for (Cliente cliente : clienti) {
+			if (cliente != null)
+			System.out.println(cliente.getNome());
+		}
+	}
+	
+	public void reportConti() {
+		System.out.println("Elenco conti aggiornato");
+		for (ContoCorrente conto : conti) {
+			if (conto != null) {
+				System.out.print(conto.dammiNumeroConto() + " - ");
+				System.out.println(conto.getCliente().getNome());
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Prj07_GestioneBanca/src/view/Gestione.java
+++ Prj07_GestioneBanca/src/view/Gestione.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.BancaController;
 import model.Cliente;
 import model.ContoCorrente;
 
@@ -7,23 +8,17 @@ public class Gestione {
 
 	public static void main(String[] args) {
 		
-		ContoCorrente[] conti = new ContoCorrente[5];
+		BancaController bc = new BancaController();
+		bc.addCliente("fabrizio", "galfré");
+		bc.addCliente("giorgio", "depetris");
+		int i = bc.trovaCliente("giorgio", "depetris");
+		Cliente c = bc.trovaCliente(i);
+		bc.addConto(c);
+		bc.reportClienti();
+		bc.reportConti();
 		
-		conti[0] = new ContoCorrente(new Cliente("Thomas", "Passafiume"));
-		conti[1] = new ContoCorrente(new Cliente("Gabriele", "Marzano"));
 		
-		conti[0].versamento(1000);
-		System.out.println(conti[0].dammiSaldo());
-		conti[0].prelievo(100);
-		System.out.println(conti[0].dammiSaldo());
-		System.out.println(conti[0].dammiNumeroConto());
-		System.out.println(conti[0].getCliente().getNome());
 		
-		conti[1].versamento(500.0);
-		System.out.println(conti[1].dammiSaldo());
-		conti[1].prelievo(100.0);
-		System.out.println(conti[1].dammiSaldo());
-		System.out.println(conti[1].dammiNumeroConto());
 		
 
 	}
