880ef3594c84b88a9a976f804e8bb8ca5c8f72fa
maboglia <mauro.bogliaccino@gmail.com>
2021-10-13 12:50:21 +0200

    agg

--- Prj08_Studenti/src/view/GestioneAula.java
+++ Prj08_Studenti/src/view/GestioneAula.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import controller.StudenteCtrl;
 import model.Studente;
 
@@ -9,11 +12,34 @@ public class GestioneAula {
 		
 		StudenteCtrl controller = new StudenteCtrl();
 		
-		for (Studente s:controller.getRegistro()) {
-			System.out.println(s.getCognome());
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<Studente> presenti = new ArrayList<>();
+		ArrayList<Studente> assenti = new ArrayList<>();
+		
+		for (Studente s : controller.getRegistro()) {
+			System.out.print ("E' presente lo studente ");
+			System.out.println(s.getCognome() + "?");
+			
+			String risposta = sc.nextLine();
+			
+			if (risposta.equalsIgnoreCase("s")) {
+				presenti.add(s);
+			} else {
+				assenti.add(s);
+			}
+			
 		}
 		
+		System.out.println("sono presenti " + presenti.size() + " studenti");
+		for (Studente studente : presenti) {
+			System.out.println("Cognome: " + studente.getCognome());
+		}
 		
+		System.out.println("sono assenti " + assenti.size() + " studenti");
+		for (Studente studente : assenti) {
+			System.out.println("Cognome: " + studente.getCognome());
+		}
 	}
 
 }
