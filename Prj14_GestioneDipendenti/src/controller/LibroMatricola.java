681f4a6aa7324d74fa31f41c5cf35e4ad564c363
maboglia <mauro.bogliaccino@gmail.com>
2021-10-22 12:41:43 +0200

    agg

---
+++ Prj14_GestioneDipendenti/src/controller/LibroMatricola.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Dipendente;
+
+public class LibroMatricola {
+
+	public static ArrayList<Dipendente> dipendenti = new ArrayList<>();
+	
+}
