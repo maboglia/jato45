4f2e35dd74e0bc2dadec7bb258de199495e14499
maboglia <mauro.bogliaccino@gmail.com>
2021-11-29 10:10:08 +0100

    agg db alimenti

---
+++ spring-alimenti-1/src/main/java/com/bogliaccino/alimenti/SpringAlimenti1Application.java
@@ -0,0 +1,13 @@
+package com.bogliaccino.alimenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlimenti1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlimenti1Application.class, args);
+	}
+
+}
---
+++ spring-alimenti-1/src/test/java/com/bogliaccino/alimenti/SpringAlimenti1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogliaccino.alimenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlimenti1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
